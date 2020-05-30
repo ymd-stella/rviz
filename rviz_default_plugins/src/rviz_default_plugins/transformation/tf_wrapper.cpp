f0442de8486089d92d16fb599c1b983e1f4ddf35
ymd-stella <world.applepie@gmail.com>
2020-05-30 17:44:18 +0900

    Use dedicated TransformListener thread
    Signed-off-by: ymd-stella <world.applepie@gmail.com>

--- rviz_default_plugins/src/rviz_default_plugins/transformation/tf_wrapper.cpp
+++ rviz_default_plugins/src/rviz_default_plugins/transformation/tf_wrapper.cpp
@@ -123,8 +123,13 @@ void TFWrapper::initialize(
   bool using_dedicated_thread)
 {
   initializeBuffer(clock, rviz_ros_node.lock()->get_raw_node(), using_dedicated_thread);
-  tf_listener_ = std::make_shared<tf2_ros::TransformListener>(
-    *buffer_, rviz_ros_node.lock()->get_raw_node(), false);
+  if (using_dedicated_thread) {
+    tf_listener_ = std::make_shared<tf2_ros::TransformListener>(
+      *buffer_, true);
+  } else {
+    tf_listener_ = std::make_shared<tf2_ros::TransformListener>(
+      *buffer_, rviz_ros_node.lock()->get_raw_node(), false);
+  }
 }
 
 void TFWrapper::initializeBuffer(
